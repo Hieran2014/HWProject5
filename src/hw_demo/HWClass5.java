5fb16a54e2d2fd0731f7ded478f45b21c298e9d0
GYTumlisan <gymoa2011@gmail.com>
2023-04-05 21:49:05 -0700

    Initial commit repository 5

---
+++ src/hw_demo/HWClass5.java
@@ -0,0 +1,11 @@
+package hw_demo;
+
+public class HWClass5 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Local repository 5....");
+		
+		System.out.println("Remote repository 5....");
+	}
+}
